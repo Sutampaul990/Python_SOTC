96f32196540f9579566f3118e6af2cfc5bc663a3
Atanu Paul <atanupaul2411@gmail.com>
2022-10-06 11:55:33 +0530

    updated code

--- Python Basics/Calculator/Calculator.py
+++ Python Basics/Calculator/Calculator.py
@@ -5,12 +5,12 @@ num2 = int(input("Enter the 2nd Number: "))
 sign = input("Enter the operation [+, -, *, /]: ")
 
 if sign == "+":
-    print("Addition Result: ", num1 + num2)
+    print("Addition is = ", num1 + num2)
 elif sign == "-":
-    print("Subtracton Result: ", num1 - num2)
+    print("Subtracton is = ", num1 - num2)
 elif sign == "*":
-    print("Multiplication Result: ", num1 * num2)
+    print("Multiplication is = ", num1 * num2)
 elif sign == "/":
-    print("Division Result: ", num1 / num2)
+    print("Division is = ", num1 / num2)
 else:
-    print("It's an Invalid Operator")
+    print("Choose correct Operator")
